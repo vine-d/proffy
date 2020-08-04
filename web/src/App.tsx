9da0a2208f8f6af3de96035259adc8534ff47d72
vine <vinicius.dutra@gmail.com>
2020-08-04 00:08:06 -0300

    first commit

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './assets/styles/global.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Proffy hi =)</h1>
+      <button>Click me</button>
+    </div>
+  );
+}
+
+export default App;
