d838d74bab1057459ae40a172d23692ceb7898f7
Vinícius Dutra <vinicius.dutra@gmail.com>
2020-08-06 16:47:02 -0300

    teacher form styles

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import warningIcon from '../../assets/images/icons/warning.svg'
+import './styles.css';
 
 function TeacherForm() {
 	return (
 		<div id="page-teacher-form" className="container">
-			<PageHeader title="Que incrível que você quer dar aulas."/>
+			<PageHeader
+				title="Que incrível que você quer dar aulas."
+				description="O primeiro passo é preencher este formulário de inscrição."
+			/>
+
+			<main>
+				<fieldset>
+					<legend>Seus dados</legend>
+					<Input name="name" label="Nome Completo" />
+					<Input name="avatar" label="Avatar" />
+					<Input name="whatsapp" label="Whatsapp" />
+				</fieldset>
+
+				<fieldset>
+					<legend>Sobre a aula</legend>
+					<Input name="subject" label="Matéria" />
+					<Input name="cost" label="Valor por hora" />
+				</fieldset>
+
+				<fieldset>
+					<legend>Horários disponíveis</legend>
+					<Input name="week_day" label="Dia da semana" />
+					<Input name="from" label="Das" />
+					<Input name="to" label="Até" />
+				</fieldset>
+
+				<footer>
+					<p>
+						<img src={warningIcon} alt="Atenção"/>
+						Aviso importante <br />
+						Preencha todos os dados!
+					</p>
+					<button type="button">Salvar</button>
+				</footer>
+			</main>
+
 		</div>
 	)
 }
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 
 
@@ -10,18 +11,9 @@ function TeacherList() {
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Estes são os proffys disponíveis.">
 				<form id="search-teachers">
-					<div className="input-block">
-						<label htmlFor="subject">Matéria</label>
-						<input type="text" id="subject" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="week_day">Dia da Semana</label>
-						<input type="text" id="week_day" />
-					</div>
-					<div className="input-block">
-						<label htmlFor="time">Horário</label>
-						<input type="text" id="time" />
-					</div>
+					<Input name="subject" label="Matéria" />
+					<Input name="week_day" label="Dia da semana" />
+					<Input type="time" name="time" label="Horário" />
 				</form>
 			</PageHeader>
 
