fd4e08592ed434f229b2e724aade9f1a971d7f1b
Vinícius Dutra <vinicius.dutra@gmail.com>
2020-08-07 12:56:32 -0300

    Teacher List backend integration

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,32 +2,41 @@ import React from 'react';
 import './styles.css';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-function TeacherItem() {
+export interface Teacher {
+	id: number
+	name: string
+	avatar: string
+	bio: string
+	subject: string
+	cost: number
+	whatsapp: string
+}
+interface teacherItemProps {
+	teacher: Teacher
+}
+
+const TeacherItem: React.FC<teacherItemProps> = ({ teacher }) => {
 	return (
 		<article className="teacher-item">
 			<header>
-				<img src="https://avatars3.githubusercontent.com/u/1598657?s=460&u=8aea005900b0d02ff50d20f53db8c9d51e2b17b3&v=4" alt="ProffyName"/>
+				<img src={teacher.avatar} alt="Avatar"/>
 				<div>
-					<strong>Proffy Name</strong>
-					<span>Química</span>
+					<strong>{teacher.name}</strong>
+					<span>{teacher.subject}</span>
 				</div>
 			</header>
 
-			<p>
-				Entusiasta das QUIMICA avançada.
-				<br /><br />
-				Diversos experimentos já sairam de controle e ocasionaram grandes estragos para muitas outras pessoas, que seriam outros serem também. Cuidado!
-			</p>
+			<p>{teacher.bio}</p>
 
 			<footer>
 				<p>
 					Preço/hora
-					<strong>R$50,00</strong>
+					<strong>{teacher.cost}</strong>
 				</p>
-				<button type="button">
+				<a href={`https://wa.me/${teacher.whatsapp}`}>
 					<img src={whatsappIcon} alt="Whatsapp" />
 					Entrar em contato
-				</button>
+				</a>
 			</footer>
 		</article>
 	)
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import './styles.css';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import Input from '../../components/Input';
 import Select from '../../components/Select';
-
-
+import api from '../../services/api';
 
 function TeacherList() {
+
+	const [subject,setSubject] = useState('')
+	const [week_day,setWeek_day] = useState('')
+	const [time,setTime] = useState('')
+	const [teachers,setTeachers] = useState([])
+
+	async function searchTeachers(e:FormEvent) {
+		e.preventDefault()
+
+		const response = await api.get('classes', {
+			params: {
+				subject,
+				week_day,
+				time
+			}
+		})
+		setTeachers(response.data)
+	}
+
 	return (
 		<div id="page-teacher-list" className="container">
 			<PageHeader title="Estes são os proffys disponíveis.">
-				<form id="search-teachers">
+				<form id="search-teachers" onSubmit={searchTeachers}>
 					<Select
 						name="subject"
 						label="Matéria"
+						value={subject}
+						onChange={e=>{
+							setSubject(e.target.value)
+							searchTeachers(e)
+						}}
 						options={[
 							{ value: 'Artes', label: 'Artes' },
 							{ value: 'Matemática', label: 'Matemática' },
@@ -28,6 +51,11 @@ function TeacherList() {
 					<Select
 						name="week_day"
 						label="Dia da semana"
+						value={week_day}
+						onChange={e=>{
+							setWeek_day(e.target.value)
+							searchTeachers(e)
+						}}
 						options={[
 							{ value: '1', label: 'Segunda' },
 							{ value: '2', label: 'Terça' },
@@ -38,15 +66,23 @@ function TeacherList() {
 							{ value: '0', label: 'Domingo' },
 						]}
 					/>
-					<Input type="time" name="time" label="Horário" />
+					<Input
+						type="time"
+						name="time"
+						label="Horário"
+						value={time}
+						onChange={e=>{
+							setTime(e.target.value)
+							searchTeachers(e)
+						}}
+					/>
 				</form>
 			</PageHeader>
 
 			<main>
-				<TeacherItem />
-				<TeacherItem />
-				<TeacherItem />
-				<TeacherItem />
+				{teachers.map((teacher:Teacher) => {
+					return <TeacherItem key={teacher.id} teacher={teacher} />
+				})}
 			</main>
 		</div>
 	)
