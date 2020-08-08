9694b181df8d3f9341b1fecb4bf623dfa41533e4
Vinícius Dutra <vinicius.dutra@gmail.com>
2020-08-08 11:30:55 -0300

    whatsapp deeplinking

--- mobile/src/components/TeacherItem/index.tsx
+++ mobile/src/components/TeacherItem/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Linking } from 'react-native';
 
 import styles from './styles';
 import { RectButton } from 'react-native-gesture-handler';
@@ -22,6 +22,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
+
+	function handleLinkToWhatsapp(){
+		Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`)
+	}
+
 	return (
 		<View style={styles.container} >
 			<View style={styles.profile}>
@@ -48,7 +53,7 @@ const TeacherItem:React.FC<TeacherItemProps> = ({ teacher }) => {
 				<RectButton style={[styles.favoriteButton, styles.favorited]}>
 					<Image source={heartOutlineIcon} />
 				</RectButton>
-				<RectButton style={styles.contactButton}>
+				<RectButton onPress={handleLinkToWhatsapp} style={styles.contactButton}>
 					<Image source={whatsappIcon} />
 					<Text style={styles.contactButtonText}>Entrar em contato</Text>
 				</RectButton>
